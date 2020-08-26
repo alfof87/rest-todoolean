30a914fad2fcf06891d50e5fc6aeb5723c94253e
Alfonso <alfof87@gmail.com>
2020-08-26 19:03:40 +0200

    rest-todoolean

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ function addInsertListener(){
 function postElements(){
   var inpText = $("#input-text");
   var text = inpText.val();
-  console.log(text);
+  // console.log(text);
   $.ajax({
     url: "http://157.230.17.132:3008/todos",
     method:  "POST",
@@ -53,9 +53,24 @@ function postElements(){
     }
   });
 }
-
-
+//????????????????
+function printTasks(tasks){
+  var target = $("#tasks");
+  target.text(" ");
+  for (var i = 0; i < tasks.length; i++) {
+    var task = tasks[i]
+    target.append(`<li>${task.text}</li>`);
+  }
+}
+//????????????????????
+// function deletelistener(){
+//   var x = $("<span>x</span>");
+//   x.click(function(){
+//     `<li>${task.text}<span>x</span></li>`.empty();
+//   })
+// }
 function init(){
+// deletelistener();
 addInsertListener();
 postElements();
 getElements();
